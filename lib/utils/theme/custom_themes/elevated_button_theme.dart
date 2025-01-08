1a568e7573ae7b18e6070e21d45f73f7b68ed368
Duy Truong <truongbaoduy2003@gmail.com>
2025-01-08 17:05:15 +0700

    Add onboarding screen, a bunch of themes

---
+++ lib/utils/theme/custom_themes/elevated_button_theme.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class TElevatedButtonTheme {
+  TElevatedButtonTheme._();
+
+  static final lightElevatedButtonTheme = ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+    elevation: 0,
+    foregroundColor: Colors.white,
+    backgroundColor: Colors.green,
+    disabledForegroundColor: Colors.grey,
+    disabledBackgroundColor: Colors.grey,
+    side: const BorderSide(color: Colors.green),
+    padding: const EdgeInsets.symmetric(vertical: 18),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
+    textStyle: const TextStyle(
+        fontSize: 16, color: Colors.white, fontWeight: FontWeight.w600),
+  ));
+
+  static final darkElevatedButtonTheme = ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+    elevation: 0,
+    foregroundColor: Colors.white,
+    backgroundColor: Colors.green,
+    disabledForegroundColor: Colors.grey,
+    disabledBackgroundColor: Colors.grey,
+    side: const BorderSide(color: Colors.green),
+    padding: const EdgeInsets.symmetric(vertical: 18),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
+    textStyle: const TextStyle(
+        fontSize: 16, color: Colors.white, fontWeight: FontWeight.w600),
+  ));
+}
